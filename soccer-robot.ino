0fb9683b86aee490fc3f3480aad0b7d09650adb4
James Ross <James.Ross@sac.on.ca>
2018-10-20 15:16:44 -0400

    Format, change moveRobotHeading

--- soccer-robot.ino
+++ soccer-robot.ino
@@ -7,25 +7,32 @@ AF_DCMotor backLeftMotor(3);
 AF_DCMotor backRightMotor(4);
 AF_DCMotor motors[4] = {frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor};
 
-int grayScale=7;
+int grayScale = 7;
 
-void moveRobot(int xSpeed, int ySpeed){
-  
+void moveRobot(int xSpeed, int ySpeed)
+{
+
   float y = map(ySpeed, 0, 255, 0, 180) * sqrt(2);
   float x = map(xSpeed, 0, 255, 0, 180) * sqrt(2);
-  float m0_2 = y + (x/2);
-  float m1_3 = y - (x/2);
-  if(m1_3 < 0){
+  float m0_2 = y + (x / 2);
+  float m1_3 = y - (x / 2);
+  if (m1_3 < 0)
+  {
     motors[1].run(BACKWARD);
     motors[3].run(BACKWARD);
-  }else{
+  }
+  else
+  {
     motors[1].run(FORWARD);
     motors[3].run(FORWARD);
   }
-  if(m0_2 < 0){
+  if (m0_2 < 0)
+  {
     motors[1].run(BACKWARD);
     motors[3].run(BACKWARD);
-  }else{
+  }
+  else
+  {
     motors[1].run(FORWARD);
     motors[3].run(FORWARD);
   }
@@ -35,56 +42,66 @@ void moveRobot(int xSpeed, int ySpeed){
   motors[3].setSpeed(abs(m1_3));
 }
 
-//moves robot in 360 degree direction with heading
-void moveRobotHeading(int heading, int str){
-  float x = cos(heading * (PI/180)) * str;
-  float y = sin(heading * (PI/180)) * str;
-  moveRobot(x,y);
+//moves robot in 360 degree direction with heading.
+void moveRobotHeading(int heading, int str)
+{
+  float x = cos((-heading + 90) * (PI / 180)) * str; //offset so 0 is the front of the robot, and goes clockwise
+  float y = sin((heading + 90) * (PI / 180)) * str;
+  moveRobot((int)x, (int)y);
 }
 
-void stopRobot(){
+void stopRobot()
+{
   moveRobot(0, 0);
 }
 
-class Grayscale{
-  private:
-    int result;
-    int pin;
- 
-  public:
-    Grayscale(int pin_num);  
-    int readShade();
+class Grayscale
+{
+private:
+  int result;
+  int pin;
+
+public:
+  Grayscale(int pin_num);
+  int readShade();
 };
 
-Grayscale::Grayscale(int pin_num){
-  pin=pin_num;
+Grayscale::Grayscale(int pin_num)
+{
+  pin = pin_num;
 }
-int Grayscale::readShade(){
+int Grayscale::readShade()
+{
   return analogRead(pin);
 }
 
 //from https://www.arduino.cc/en/Tutorial/Ping
-long microsecondsToCentimeters(long microseconds) {
+long microsecondsToCentimeters(long microseconds)
+{
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
   // The ping travels out and back, so to find the distance of the object we
   // take half of the distance travelled.
   return microseconds / 29 / 2;
 }
 
-class PingSensor{
-  private:
-    int pin;
-    long distance;
-  public:
-    PingSensor(int pin_num);
-    long readDist();
+class PingSensor
+{
+private:
+  int pin;
+  long distance;
+
+public:
+  PingSensor(int pin_num);
+  long readDist();
 };
 
-PingSensor::PingSensor(int pin_num){
+PingSensor::PingSensor(int pin_num)
+{
   pin = pin_num;
 }
 
-long PingSensor::readDist(){
+long PingSensor::readDist()
+{
   pinMode(pin, OUTPUT);
   digitalWrite(pin, LOW);
   delayMicroseconds(2);
@@ -96,45 +113,32 @@ long PingSensor::readDist(){
   return distance;
 }
 
-class IRSensor{
-  
-  public:
-    IRSensor(){
-      
-    }
+class IRSensor
+{
+
+public:
+  IRSensor()
+  {
+  }
 };
-//  delay(2000);
-//  FL.setSpeed(0); 
 
-void testMotors(){
-  
-//  FL.setSpeed(250);
-//  FL.run(FORWARD);
-//  FR.setSpeed(250);
-//  FR.run(FORWARD);
-//  delay(2000);
-//  FR.setSpeed(0);
-//  BL.setSpeed(250);
-//  BL.run(FORWARD);
-//  delay(2000);
-//  BL.setSpeed(0);
-//  BR.setSpeed(250);
-//  BR.run(FORWARD);
-//  delay(2000);
-//  BR.setSpeed(0);
+void testMotors()
+{
+  moveRobotHeading(0, 100);
+  moveRobotHeading(90, 100);
+  moveRobotHeading(180, 100);
+  moveRobotHeading(270, 100);
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(250000);           // set baud rate to 250k
+void setup()
+{
+  Serial.begin(250000); // set baud rate to 250k
   Serial.println("Motor test!");
-  
-  // turn on motor
-//  testMotors(); 
 
+  testMotors();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-
 }
